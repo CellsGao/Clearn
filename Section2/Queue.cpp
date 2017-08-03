6496314b13d8a8362702aec15ea8ff8ac2dcf7ed
Cellsgao <326422808@qq.com>
2017-08-04 00:20:22 +0800

    一些改变

--- Section2/Queue.cpp
+++ Section2/Queue.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <iostream>
 
-struct QNode{ //队列中的节点
+struct QNode { //队列中的节点
     int Data;
     struct QNode *Next;
 };
@@ -13,69 +13,70 @@ typedef struct QNode *QueuePtr;
 typedef struct {
     QueuePtr front;
     QueuePtr rear;
-}*LinkQueue;
+} *LinkQueue;
 
 //初始化队列
-void initQueue(LinkQueue q){
-    QueuePtr s = (QueuePtr)malloc(sizeof(QNode));   //定义一个头结点
-    s->Next=NULL;
-    q->front=q->rear=s;
+void initQueue(LinkQueue q) {
+    QueuePtr s = (QueuePtr) malloc(sizeof(QNode));   //定义一个头结点
+    s->Next = NULL;
+    q->front = q->rear = s;
 }
 
 //插入元素
-void insertElem(LinkQueue q,int elem){
+void insertElem(LinkQueue q, int elem) {
     QueuePtr temp;
-    temp=(QueuePtr)malloc(sizeof(QueuePtr));
-    if(temp){
-        temp->Data=elem;
-        temp->Next=NULL;
-        q->rear->Next=temp; //主要是使front的Next指针可以向下指
-        q->rear=temp;
+    temp = (QueuePtr) malloc(sizeof(QueuePtr));
+    if (temp) {
+        temp->Data = elem;
+        temp->Next = NULL;
+        q->rear->Next = temp; //主要是使front的Next指针可以向下指
+        q->rear = temp;
     }
 
 }
+
 //判断是否为空
-bool isEmpty(LinkQueue q){
-    return (q->front==NULL);
+bool isEmpty(LinkQueue q) {
+    return (q->front == NULL);
 }
 
 //删除元素
-int delElem(LinkQueue q){
+int delElem(LinkQueue q) {
     int elem;
-    if(isEmpty(q)){
+    if (isEmpty(q)) {
         printf("队列为空");
-    }else{
+    } else {
         QueuePtr temp;
-        temp=q->front->Next;
-        if(q->front==q->rear){ //队列只有一个元素
-            q->front=q->rear=NULL;
-        } else{
-            elem=temp->Data;
-            q->front->Next=temp->Next;
+        temp = q->front->Next;
+        if (q->front == q->rear) { //队列只有一个元素
+            q->front = q->rear = NULL;
+        } else {
+            elem = temp->Data;
+            q->front->Next = temp->Next;
             free(temp);
             return elem;
         }
     }
 }
 
-int printQueue(LinkQueue q){
-    if(q->rear==q->front){
+int printQueue(LinkQueue q) {
+    if (q->rear == q->front) {
         printf("队列满");
-    } else{
-        QueuePtr temp=q->front->Next;
-        while (temp){
-            printf("%d ",temp->Data);
-            temp=temp->Next;
+    } else {
+        QueuePtr temp = q->front->Next;
+        while (temp) {
+            printf("%d ", temp->Data);
+            temp = temp->Next;
         }
     }
     printf("\n");
 }
 
-int main(){
+int main() {
     LinkQueue q;
     initQueue(q);
-    for(int i=0;i<10;i++){
-        insertElem(q,i);
+    for (int i = 0; i < 10; i++) {
+        insertElem(q, i);
     }
     printQueue(q);
     delElem(q);
--- Section3/BinTree.cpp
+++ Section3/BinTree.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <stdio.h>
 
-struct TreeNode{ //数节点定义
+struct TreeNode { //数节点定义
     int Data;
     struct TreeNode *right;
     struct TreeNode *left;
@@ -17,84 +17,84 @@ typedef TreeNode *BinTree;
  */
 
 typedef struct SNode *Stack;
-struct SNode{
+struct SNode {
     int Data;
     struct SNode *Next;
 };
 
-Stack creatStack(){
+Stack creatStack() {
     //创建一个头结点,空栈
     Stack s;
-    s=(Stack)malloc(sizeof(struct SNode));
-    s->Next=NULL;
+    s = (Stack) malloc(sizeof(struct SNode));
+    s->Next = NULL;
     return s;
 }
 
-int isEmpty(Stack s){
-    return (s->Next==NULL);
-}
-void push(Stack s,int item){
-    //将元素压入堆栈s（头结点之后）
-    Stack tepCell;
-    tepCell=(Stack)malloc(sizeof(struct SNode));
-    tepCell->Data=item;
-    tepCell->Next=s->Next;
-    s->Next=tepCell;
+int isEmpty(Stack s) {
+    return (s->Next == NULL);
 }
 
-int pop(Stack s){
+void push(Stack s, int item) {
+    //将元素压入堆栈s（头结点之后）
+    Stack tepCell;
+    tepCell = (Stack) malloc(sizeof(struct SNode));
+    tepCell->Data = item;
+    tepCell->Next = s->Next;
+    s->Next = tepCell;
+}
+
+int pop(Stack s) {
     Stack firstCell;
-    if(isEmpty(s)){
+    if (isEmpty(s)) {
         printf("栈空");
-    } else{
-        firstCell=s->Next;
-        s->Next=firstCell->Next;
-        int topElement=firstCell->Data;
+    } else {
+        firstCell = s->Next;
+        s->Next = firstCell->Next;
+        int topElement = firstCell->Data;
         free(firstCell);
         return topElement;
     }
 }
 
 
-
 //先序遍历递归 ：先根节点，再左子树后右子树
-void PreOrderTraversal(BinTree bt){
-    if(bt){
-        printf("%d",bt->Data);
+void PreOrderTraversal(BinTree bt) {
+    if (bt) {
+        printf("%d", bt->Data);
         PreOrderTraversal(bt->left);
         PreOrderTraversal(bt->right);
     }
 }
 
 //中序遍历递归：先左子树，后根节点，最后右子树.
-void InOrderTraversal(BinTree bt){
-    if(bt){
+void InOrderTraversal(BinTree bt) {
+    if (bt) {
         InOrderTraversal(bt->left);
-        printf("%d",bt->Data);
+        printf("%d", bt->Data);
         InOrderTraversal(bt->right);
     }
 }
 
 //后序遍历递归：先左右后，最后根节点
-void PostOrderTraversal(BinTree bt){
-    if(bt){
+void PostOrderTraversal(BinTree bt) {
+    if (bt) {
         PostOrderTraversal(bt->left);
         PostOrderTraversal(bt->right);
-        printf("%d",bt->Data);
+        printf("%d", bt->Data);
     }
 }
 
 //使用堆栈进行中序非递归遍历
-void traversalInOrder(BinTree bt){
-    Stack s=creatStack();
-    BinTree t=bt;
-    while (t||isEmpty(s)){
-        push(s,t->Data);
-        t=t->left;
+void traversalInOrder(BinTree bt) {
+    Stack s = creatStack();
+    BinTree t = bt;
+    while (t || isEmpty(s)) {
+        push(s, t->Data);
+        t = t->left;
     }
-    if(!isEmpty(s)){
-        t=(BinTree)pop(s);
-        printf("%d",t->Data);
-        t=t->right;
+    if (!isEmpty(s)) {
+        t = (BinTree) pop(s);
+        printf("%d", t->Data);
+        t = t->right;
     }
 }
