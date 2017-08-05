b3909c2652b93ffe61c108ecd4bd02b7d880af32
Cellsgao <326422808@qq.com>
2017-08-06 01:30:44 +0800

    冒泡

--- bubble/bubbleSort.cpp
+++ bubble/bubbleSort.cpp
@@ -11,18 +11,18 @@
 int main() {
     int a[] = {3, 4, 5, 21, 6, 4};
     for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 6-i; j++) {
+        for (int j = 0; j < 6 - i; j++) {
             if (a[j] > a[j + 1]) {
                 int temp;
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
 
     }
-    for (int k = 0; k <6 ; k++) {
-        printf("%d",a[k]);
+    for (int k = 0; k < 6; k++) {
+        printf("%d", a[k]);
         printf(" ");
     }
 
